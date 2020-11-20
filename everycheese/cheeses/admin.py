cb707ca760caa5af61b81ce7a6cb1267ef1e058c
bugismylife <sergenucar1718@gmail.com>
2020-11-20 13:15:07 +0100

    Register Cheese model with the admin

--- everycheese/cheeses/admin.py
+++ everycheese/cheeses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cheese
 
-# Register your models here.
+
+@admin.register(Cheese)
+class CheeseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'firmness', 'slug', 'created','modified',)
+    list_filter = ('firmness','created',)
+    search_fields = ('name',)
+    date_hierarchy = 'created'
+    ordering = ('created','modified',)
